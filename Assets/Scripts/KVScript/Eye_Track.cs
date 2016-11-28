dea7f0ab5ee826677df4a5017e6d9fd78278f74f
kavehes <kvsagot@gmail.com>
2016-11-29 00:41:58 +0100

    Flood mode enabled

---
+++ Assets/Scripts/KVScript/Eye_Track.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eye_Track : MonoBehaviour {
+
+	public Transform Anchor;
+	public float maxDistance = 0.5f;
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Anchor.position + (Target.position - Anchor.position).normalized*maxDistance;
+	}
+}
