dab77ced865d054e92b43fe1538a03aaf9ef8dba
kavehes <kvsagot@gmail.com>
2016-11-28 17:42:38 +0100

    Moar Stuff

--- Assets/Scripts/KVScript/TestingSpine.cs
+++ Assets/Scripts/KVScript/TestingSpine.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Spine.Unity;
-public class TestingSpine : MonoBehaviour {
 
+public class TestingSpine : MonoBehaviour
+{
+
+	SkeletonAnimation skel;
+
+	[SpineAttachment (currentSkinOnly: true, slotField: "Paupiere")]
+	public string eyesOpen;
+
+	[SpineAttachment (currentSkinOnly: true, slotField: "Paupiere")]
+	public string eyesClosed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		skel = GetComponent<SkeletonAnimation> ();
+		StartCoroutine (Blink ());
+		//ça ça gère la vitesse d'anim (1 pour 100%)
+		skel.AnimationState.TimeScale = 0.75f;
+		//On met une animation sur un track, 0 prenant la priorité sur toutes les autres animations, le nom de l'animation puis si ça loop ou pas
+		skel.AnimationState.SetAnimation (1, "FlapAiles", true);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	IEnumerator Blink ()
+	{
+		while (true) {
+			//Voilà comment on change l'image attaché à un Slot particulier
+			//Il faut rentrer le 
+			skel.skeleton.SetAttachment ("Paupiere", eyesClosed);
+			yield return new WaitForSeconds (3);
+			skel.skeleton.SetAttachment ("Paupiere", eyesOpen);
+			yield return new WaitForSeconds (3);
+		}
 	}
+
 }
