ae43124ce8c21a859bb250ac930dd97e2e93de69
kavehes <kvsagot@gmail.com>
2016-12-01 16:29:21 +0100

    Des trucs (Ctrl+S)
    prout prout prout

---
+++ Assets/Scripts/KVScript/GargScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GargScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		GameObject[] chars = GameObject.FindGameObjectsWithTag("Player");
+		float tmpDist = 900;
+		int choice = 0;
+		for (int i = 0; i < chars.Length; i++) {
+			if(Vector3.Distance(chars[i].transform.position, transform.position)<tmpDist){
+				choice = i;
+				tmpDist = Vector3.Distance(chars[i].transform.position, transform.position);
+			}
+		}
+		Debug.Log (choice);
+		Quaternion tmpRot = Quaternion.FromToRotation (Vector3.down, chars [choice].transform.position - transform.position);
+		transform.rotation = tmpRot;
+		Debug.Log (tmpRot.eulerAngles);
+	}
+}
