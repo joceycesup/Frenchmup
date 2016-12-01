7895d3f02a69181e1719426715bd5423ea7c35d6
Thibault Baron <thibault.baron56@gmail.com>
2016-12-01 21:44:44 +0100

    Boutons reassignes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,6 +56,24 @@ public class Player : Character {
 	protected override void AwakeCharacter () {
 		//gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Sprites/Rocket"+playerNumber);
 		_isEnemy = false;
+		if (laser == null) {
+			laser = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/Laser"));
+			laser.GetComponent<Laser> ().Stop ();
+			laser.transform.parent = transform;
+			laser.transform.position = transform.position;
+		}
+		if (smartBomb == null) {
+			smartBomb = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/SmartBomb"));
+			smartBomb.SetActive (false);
+			smartBomb.transform.parent = transform;
+			smartBomb.transform.position = transform.position;
+		}
+		if (magnet == null) {
+			magnet = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/Magnet"));
+			magnet.SetActive (false);
+			magnet.transform.parent = transform;
+			magnet.transform.position = transform.position;
+		}
 	}
 
 	void Start () {
@@ -69,6 +87,8 @@ public class Player : Character {
 		if (!GameSettings.tutorial) {
 			laserLoad = maxLaserLoad;
 		}
+		magnet.SetActive (false);
+		SetCanShoot (false);
 	}
 
 	protected override void UpdateCharacter () {
@@ -109,11 +129,11 @@ public class Player : Character {
 			speed = dashSpeed;
 			if (IngameTime.time >= dashEndTime) {
 				dash = false;
-				transform.GetChild(1).GetComponent<CircleCollider2D> ().radius = 0.18f;
+				//transform.GetChild(1).GetComponent<CircleCollider2D> ().radius = 0.18f;
 				speed = state == PlayerState.DPS ? dpsSpeed : supportSpeed;
 			}
 		}
-		if (laser != null && laser.GetComponent<Laser> ().isActiveAndEnabled) {
+		if (laser.GetComponent<Laser> ().isActiveAndEnabled) {
 			speed = laserSpeed;
 			if ((laserLoad -= maxLaserLoad * IngameTime.deltaTime / laserUnloadTime) <= 0f) {
 				laserLoad = 0f;
@@ -123,61 +143,46 @@ public class Player : Character {
 		}
 		switch (state) {
 		case PlayerState.DPS:
-			if (Input.GetButton ("Fire1_P"+playerNumber)) {
-				SetCanShoot (true);
-			}
-			if (Input.GetButtonUp ("Fire1_P"+playerNumber)) {
-				SetCanShoot (false);
-			}
-			if (Input.GetButtonDown ("Fire2_P"+playerNumber)) {
+			if (Input.GetButtonDown ("Fire1_P"+playerNumber)) {
 				if (laserLoad > 0f) {
-					if (laser == null) {
-						laser = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/Laser"));
-						laser.transform.parent = transform;
-						laser.transform.position = transform.position;
-					}
 					laser.SetActive (true);
 					laser.GetComponent<Laser> ().Shoot ();
 				}
 			}
-			if (Input.GetButtonUp ("Fire2_P"+playerNumber)) {
+			if (Input.GetButtonUp ("Fire1_P"+playerNumber)) {
 				if (laser != null) {
 					laser.GetComponent<Laser> ().Stop ();
 				}
 				speed = dpsSpeed;
 				//laser.SetActive (false);
 			}
+			if (Input.GetButton ("Fire2_P"+playerNumber)) {
+				SetCanShoot (true);
+			}
+			if (Input.GetButtonUp ("Fire2_P"+playerNumber)) {
+				SetCanShoot (false);
+			}
 			if (Input.GetButtonDown ("Fire3_P"+playerNumber)) {
-				if (smartBomb == null) {
-					smartBomb = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/SmartBomb"));
-					smartBomb.transform.parent = transform;
-					smartBomb.transform.position = transform.position;
-				}
 				smartBomb.SetActive (true);
 			}
 			break;
 		case PlayerState.Support:
 			if (Input.GetButtonDown ("Fire1_P"+playerNumber)) {
+				magnet.SetActive (true);
+			}
+			if (Input.GetButtonUp ("Fire1_P"+playerNumber)) {
+				magnet.SetActive (false);
+			}
+			if (Input.GetButtonDown ("Fire2_P"+playerNumber) && Input.GetButton ("Fire1_P"+playerNumber)) {
 				if (!dash) {
 					if (IngameTime.time > dashEndTime + dashCooldown) {
 						dash = true;
-						transform.GetChild(1).GetComponent<CircleCollider2D> ().radius = 2;
+						//transform.GetChild (1).GetComponent<CircleCollider2D> ().radius = 2;
 						dashEndTime = IngameTime.time + dashDistance / dashSpeed;
 						speed = dashSpeed;
 						dashVector = deltaPos;
 					}
 				}
-			}
-			if (Input.GetButtonDown ("Fire2_P"+playerNumber)) {
-				if (magnet == null) {
-					magnet = (GameObject)Instantiate (Resources.Load<GameObject> ("Prefabs/Magnet"));
-					magnet.transform.parent = transform;
-					magnet.transform.position = transform.position;
-				}
-				magnet.SetActive (true);
-			}
-			if (Input.GetButtonUp ("Fire2_P"+playerNumber)) {
-				magnet.SetActive (false);
 			}
 			if (Input.GetButtonDown ("Fire3_P"+playerNumber)) {
 				if (!bulletTime) {
@@ -223,6 +228,7 @@ public class Player : Character {
 		} else {
 			StartCoroutine ("RespawnPlayer", this);
 		}
+		Reset ();
 		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
 		this.enabled = false;
 	}
