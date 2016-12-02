4f7ab7493cd511752801b51f8365ed242321d31f
Thibault Baron <thibault.baron56@gmail.com>
2016-12-02 01:09:00 +0100

    On a une chaine!!

--- Assets/Curves/SplineDecorator.cs
+++ Assets/Curves/SplineDecorator.cs
@@ -24,12 +24,12 @@ public class SplineDecorator : MonoBehaviour
 		linksT = new List<float> ();
 		extents = new Vector3[items.Length];
 		for (int i = 0; i < items.Length; ++i) {
+			//items [i].SetActive (true);
 			extents [i] = items [i].GetComponent<SpriteRenderer> ().bounds.extents * overlap;
 		}
 	}
 
-	void Start ()
-	{
+	void Start () {
 		if (precision <= 0 || items == null || items.Length == 0 || overlap <= 0.0f) {
 			return;
 		}
@@ -72,7 +72,7 @@ public class SplineDecorator : MonoBehaviour
 			item.transform.parent = spline.transform;
 		}//*/
 	}
-
+	/*
 	void Update () {
 		for (int i = 0; i < links.Count; ++i) {
 			Vector3 position = spline.GetPoint (linksT [i]);
@@ -82,5 +82,52 @@ public class SplineDecorator : MonoBehaviour
 			}
 			links [i].transform.position = position;
 		}
+	}//*/
+
+	void Update () {
+		if (precision <= 0 || items == null || items.Length == 0 || overlap <= 0.0f) {
+			return;
+		}
+
+		float t = 0.0f;
+		//float d = 0.0f;
+		//Vector3 displacement = new Vector3 ();
+		int i = 0;
+		int n = 0;
+		while (t <= 1.0f) {
+			GameObject item;
+			if (links.Count <= n) {
+				item = Instantiate (items [i]);
+				links.Add (item);
+				linksT.Add (t);
+			} else {
+				item = links [n];
+			}
+			links [n].SetActive (true);
+			Vector3 position = spline.GetPoint (t);
+			if (lookForward) {
+				Quaternion tmpRot = Quaternion.FromToRotation (Vector3.up, spline.GetDirection (t));
+				item.transform.rotation = tmpRot;
+			}
+			item.transform.position = position;
+			/*
+			d = extents [i].y;
+			float tmp = t;
+			//Vector3 dCurve = new Vector3 ();
+			for (int j = 0; j < precision; ++j) {
+				Vector3 velocity = spline.GetVelocity (tmp);
+				//dCurve += Vector3.Normalize (velocity);
+				tmp += (d / velocity.magnitude) / precision;
+			}
+			t = tmp;//*/
+			t = spline.GetT (t, extents [i].y, 10);
+
+			n++;
+			i++;
+			i %= items.Length;
+			item.transform.parent = spline.transform;
+		}//*/
+		for (; n < links.Count; ++n)
+			links [n].SetActive (false);
 	}
 }
---
+++ Assets/Scripts/Chain.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chain : MonoBehaviour {
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject links;
+
+	private BezierSpline spline;
+	private float length;
+	public float retractSpeed = 0.5f;
+	public float turnRate = 90f;
+	private GameObject transformObject;
+
+	public float damage = 2f;
+
+	void Awake () {
+		transformObject = new GameObject ("TransformObject");
+		transformObject.transform.parent = gameObject.transform;
+		spline = GetComponent<BezierSpline> ();
+		links.SetActive (false);
+		spline.points [0] = player1.transform.position;
+		spline.points [3] = player2.transform.position;
+		spline.points [1] = spline.points [2] = (player1.transform.position + player2.transform.position) / 2f;
+		length = Vector3.Distance (player1.transform.position, player2.transform.position) / 2f;
+	}
+
+	void Update () {
+		links.SetActive (player1.GetComponent<Player> ().state != player2.GetComponent<Player> ().state);
+		float tmpLength = Vector3.Distance (player1.transform.position, player2.transform.position) / 2f;
+		if (tmpLength > length) {
+			length = tmpLength;
+		} else {
+			length = Mathf.Max (tmpLength, length - retractSpeed * IngameTime.deltaTime);
+		}
+		Vector3 p1pos = spline.points [0] == player1.transform.position ? spline.points [1] : player1.transform.position;
+		Vector3 p2pos = spline.points [3] == player2.transform.position ? spline.points [2] : player2.transform.position;
+		if (player1.transform.position != spline.points [0] || tmpLength != length) {
+			Vector3 tmp1 = spline.GetVelocity (0.005f);
+			float angle = Vector3.Angle (spline.points [0] - p1pos, tmp1);
+			transformObject.transform.rotation = Quaternion.Euler (Vector3.forward * Mathf.Min (angle, turnRate * IngameTime.deltaTime) * (Vector3.Cross (spline.points [0] - p1pos, tmp1).z > 0f ? -1f : 1f));
+			spline.points [1] = player1.transform.position + transformObject.transform.TransformDirection (Vector3.Normalize (spline.points [1] - spline.points [0])) * length;
+			spline.points [0] = player1.transform.position;
+		}
+		if (player2.transform.position != spline.points [3] || tmpLength != length) {
+			Vector3 tmp2 = spline.GetVelocity (0.995f);
+			float angle = Vector3.Angle (spline.points [3] - p2pos, tmp2);
+			transformObject.transform.rotation = Quaternion.Euler (Vector3.forward * Mathf.Min (angle, turnRate * IngameTime.deltaTime) * (Vector3.Cross (spline.points [3] - p2pos, tmp2).z > 0f ? 1f : -1f));
+			spline.points [2] = player2.transform.position + transformObject.transform.TransformDirection (Vector3.Normalize (spline.points [2] - spline.points [3])) * length;
+			spline.points [3] = player2.transform.position;
+		}
+	}
+}
