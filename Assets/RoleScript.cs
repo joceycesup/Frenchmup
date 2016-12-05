cfeb3d7826dd7f3c82ee44c208050fadc5e7b79d
kavehes <kvsagot@gmail.com>
2016-12-05 05:16:06 +0100

    push

---
+++ Assets/RoleScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RoleScript : MonoBehaviour {
+
+	public Player player;
+
+	public Sprite DPS;
+	public Sprite SUP;
+
+	void Update(){
+		if (player.state == Player.PlayerState.DPS) {
+			gameObject.GetComponent<Image> ().sprite = DPS;
+		} else {
+			gameObject.GetComponent<Image> ().sprite = SUP;
+		}
+	}
+
+}
